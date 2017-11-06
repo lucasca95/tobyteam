d74aec79cad3e61739023bc28a35bb3e3d2f854a
lucasca95 <lucasca95@yahoo.com.ar>
2017-11-06 19:28:25 -0300

    modificacion archvio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'inicio/index'
+  resources:inicio
+  root :to => 'inicio#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

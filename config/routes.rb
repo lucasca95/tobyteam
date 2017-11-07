89dfdb783fd22abbf48aa1871acbbe96c0ad8660
lucasca95 <lucasca95@yahoo.com.ar>
2017-11-07 13:52:47 -0300

    Modificamos README y borramos ControladorInicio

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources:inicio
-  root :to => 'inicio#index'
+	
+	resources :main_controller
+	root :to => main_controller#inicio
+
+  get 'main/default'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

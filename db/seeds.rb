62dcaf00285874a1189976f678ca67008769c203
LihuenFigueroa <lihuenfigueroa@gmail.com>
2017-11-10 10:33:40 -0300

    Correccion seed : Notificaciones, y action_levels

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,27 +5,36 @@ University.find_or_create_by(name: "UNLP", province: "Buenos Aires", url:"unlp.e
 University.find_or_create_by(name: "UBA", province: "Capital Federal", url:"uba.edu.ar")
 
 puts "Creo Niveles"
-Level.find_or_create_by(name: "Junior", points: 10)
-Level.find_or_create_by(name: "Senior", points: 100)
+level_1=Level.find_or_create_by(name: "Junior", points: 10)
+level_2=Level.find_or_create_by(name: "Senior", points: 100)
+
+
 
 puts "Creo Acciones"
-Action.find_or_create_by(name: "Votar Positvo", points: 3)
-Action.find_or_create_by(name: "Votar Negativo", points: -1)
+action_1=Action.find_or_create_by(name: "Votar Positvo", points: 3)
+action_2=Action.find_or_create_by(name: "Votar Negativo", points: -1)
+
+level_2.actions << action_1
+level_2.actions << action_2
+level_1.actions << action_1
+
 
 puts "Creo Etiqueta"
 etiqueta_ingenieria=Label.find_or_create_by(title: "Ingenieria", active: true)
 etiqueta_electronica=Label.find_or_create_by(title: "Electronica", active: false)
+
+
+
+puts "Se crean los usuarios Lucas, Mono, Vincent"
+User.create(name: "Lucas", lastname: "Camino", email: "lucasca95@yahoo.com.ar", password:"12345678", points: 10, level_id: 1)
+User.create(name: "Lihuen", lastname: "Figueroa", email: "tumonitosexy@msn.com.ar", password:"1235678", points: 10, level_id: 1)
+User.create(name: "Vincent", lastname: "Corrao", email: "licenciado@iscl.com.mx", password:"12345678", points: 10, university_id: 1, level_id: 1)
 
 puts "Creo Notificaciones"
 Notification.find_or_create_by(body: "Notificaion 1", user_id: 3, read:true)
 Notification.find_or_create_by(body: "Notificaion 2", user_id: 3, read:false)
 Notification.find_or_create_by(body: "Notificaion 3", user_id: 3, read:false)
 Notification.find_or_create_by(body: "Notificaion 1", user_id: 2, read:false)
-
-puts "Se crean los usuarios Lucas, Mono, Vincent"
-User.create(name: "Lucas", lastname: "Camino", email: "lucasca95@yahoo.com.ar", password:"12345678", points: 10, level_id: 1)
-User.create(name: "Lihuen", lastname: "Figueroa", email: "tumonitosexy@msn.com.ar", password:"1235678", points: 10, level_id: 1)
-User.create(name: "Vincent", lastname: "Corrao", email: "licenciado@iscl.com.mx", password:"12345678", points: 10, university_id: 1, level_id: 1)
 
 puts "Creo Pregunta"
 pregunta_1=Question.find_or_create_by(title: "¿Que hace Ruby?", body: "Serian tan amables de contrame lo que hace ruby?", user_id: 3)

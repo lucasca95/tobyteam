726636d19c9bdfda025824cfa5d14611448e445a
LihuenFigueroa <lihuenfigueroa@gmail.com>
2017-11-16 19:45:02 -0300

    Historias Registro, Inicio, Cierre de sesion : FINALIZADO

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
    protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:lastname,:university_id,:level])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:lastname,:university_id, :level_id])
   end
 end

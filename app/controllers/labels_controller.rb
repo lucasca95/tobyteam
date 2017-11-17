e49b603b6df29e5edcb6cc3f33270d6c59784071
lucasca95 <lucasca95@yahoo.com.ar>
2017-11-17 04:45:51 -0300

    solucion de errores en agregar nivel. ahora pido que el usuario este logueado

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,4 +1,5 @@
 class LabelsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def new
     accion=current_user.level.actions.where(name:"Crear Etiqueta").first
     if !( accion.nil? )

7189942174e18e917688a984b6aaac0f4e026bdb
lucasca95 <lucasca95@yahoo.com.ar>
2017-11-17 04:53:33 -0300

    puse página principal de prueba para heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'bug/show'
-  root :to 'bug#show'
+    resources :bug
+  root :to => 'bug#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

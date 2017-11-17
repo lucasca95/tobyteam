dc3f98adc64e56f3ba20a61f72ae2202e89d8e5b
lucasca95 <lucasca95@yahoo.com.ar>
2017-11-17 03:24:40 -0300

    Modificamos unicidad de level

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,4 +1,13 @@
 class Level < ApplicationRecord
 	has_many :user
 	has_and_belongs_to_many :actions
+
+	#Validaciones
+	validates :body,
+		presence: true
+	validates :body,
+		numericality: true
+	validates :body,
+		uniqueness: true
+
 end

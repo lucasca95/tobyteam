e804d419eee15d9f660f61cdd65e9b28f26662a4
root <santiago@isci.com.ar>
2017-11-17 05:15:58 -0300

    Votar Pregunta y respuesta

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,4 +21,148 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  def votar_pregunta 
+    @id = params[:question]
+    @q = Question.find(@id)
+    @like = params[:like]
+    if (@like == "true")
+      @permiso = current_user.level.actions.where(name: "Votar Pregunta Positivo").first
+      if @permiso.nil?
+        redirect_to :controller => 'main', :action => 'sin_permiso'
+      else
+        @v = Vote.where(votable_id: params[:question],votable_type:"Question",user:current_user ).first
+        if @v.nil?    
+          @v = Vote.new
+          @v.votable_id = @id
+          @v.votable_type = "Question"
+          @v.like = @like
+          @v.user = current_user
+          @q.user.points += @permiso.points
+        else
+          @v.like = @like
+          current_user.points += @permiso.points
+          @q.user.points += 2*@permiso.points
+        end
+        if @v.save
+          if @q.user.save
+            if @current_user.save
+              redirect_to @q
+            else
+              redirect_to :controller => 'main', :action => 'error'
+            end
+          else
+            redirect_to :controller => 'main', :action => 'error'
+          end
+        else
+          redirect_to :controller => 'main', :action => 'error'
+        end
+
+      end
+    else 
+      @permiso = current_user.level.actions.where(name: "Votar Pregunta Negativo").first
+      if @permiso.nil?
+        redirect_to :controller => 'main', :action => 'sin_permiso'
+      else
+        @v = Vote.where(votable_id: params[:question],votable_type:"Question",user:current_user ).first
+        if @v.nil?   
+          @v = Vote.new
+          @v.votable_id = @id
+          @v.votable_type = "Question"
+          @v.like = like
+          @v.user = current_user
+          @q.user.points -= @permiso.points
+        else
+          @v.like = @like
+          current_user.points -= @permiso.points
+          @q.user.points -= 2*@permiso.points
+        end
+        if @v.save
+          if @q.user.save
+            if @current_user.save
+              redirect_to @q
+            else
+              redirect_to :controller => 'main', :action => 'error'
+            end
+          else
+            redirect_to :controller => 'main', :action => 'error'
+          end
+        else
+          redirect_to :controller => 'main', :action => 'error'
+        end
+      end
+    end
+  end
+
+  def votar_respuesta
+    @id = params[:answer]
+    @q = Answer.find(@id)
+    @like = params[:like]
+    if (@like == "true")
+      @permiso = current_user.level.actions.where(name: "Votar Respuesta Positivo").first
+      if @permiso.nil?
+        redirect_to :controller => 'main', :action => 'sin_permiso'
+      else
+        @v = Vote.where(votable_id: params[:answer],votable_type:"Answer",user:current_user ).first
+        if @v.nil?    
+          @v = Vote.new
+          @v.votable_id = @id
+          @v.votable_type = "Answer"
+          @v.like = @like
+          @v.user = current_user
+          @q.user.points += @permiso.points
+        else
+          @v.like = @like
+          current_user.points += @permiso.points
+          @q.user.points += 2*@permiso.points
+        end
+        if @v.save
+          if @q.user.save
+            if @current_user.save
+              redirect_to @q
+            else
+              redirect_to :controller => 'main', :action => 'error'
+            end
+          else
+            redirect_to :controller => 'main', :action => 'error'
+          end
+        else
+          redirect_to :controller => 'main', :action => 'error'
+        end
+
+      end
+    else 
+      @permiso = current_user.level.actions.where(name: "Votar Respuesta Negativo").first
+      if @permiso.nil?
+        redirect_to :controller => 'main', :action => 'sin_permiso'
+      else
+        @v = Vote.where(votable_id: params[:answer],votable_type:"Answer",user:current_user ).first
+        if @v.nil?   
+          @v = Vote.new
+          @v.votable_id = @id
+          @v.votable_type = "Answer"
+          @v.like = like
+          @v.user = current_user
+          @q.user.points -= @permiso.points
+        else
+          @v.like = @like
+          current_user.points -= @permiso.points
+          @q.user.points -= 2*@permiso.points
+        end
+        if @v.save
+          if @q.user.save
+            if @current_user.save
+              redirect_to @q
+            else
+              redirect_to :controller => 'main', :action => 'error'
+            end
+          else
+            redirect_to :controller => 'main', :action => 'error'
+          end
+        else
+          redirect_to :controller => 'main', :action => 'error'
+        end
+      end
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -35,15 +35,15 @@ action_21=Action.find_or_create_by(name: "Borrar Usuario", points: 0)
 action_23=Action.find_or_create_by(name: "Votar Pregunta Positivo", points: 4)
 action_24=Action.find_or_create_by(name: "Votar Respuesta Positivo", points: 5)
 action_25=Action.find_or_create_by(name: "Votar Comentario Positivo", points: 6)
-action_23=Action.find_or_create_by(name: "Votar Pregunta Negativo", points: 4)
-action_24=Action.find_or_create_by(name: "Votar Respuesta Negativo", points: 5)
-action_25=Action.find_or_create_by(name: "Votar Comentario Negativo", points: 6)
-action_26=Action.find_or_create_by(name: "Comentar Pregunta", points: 7)
-action_27=Action.find_or_create_by(name: "Comentar Respuesta", points: 8)
-action_28=Action.find_or_create_by(name: "Responder Pregunta", points: 9)
-action_29=Action.find_or_create_by(name: "Denunciar Pregunta", points: 10)
-action_30=Action.find_or_create_by(name: "Denunciar Respuesta", points: 11)
-action_31=Action.find_or_create_by(name: "Denunciar Comentario", points: 12)
+action_26=Action.find_or_create_by(name: "Votar Pregunta Negativo", points: 4)
+action_27=Action.find_or_create_by(name: "Votar Respuesta Negativo", points: 5)
+action_28=Action.find_or_create_by(name: "Votar Comentario Negativo", points: 6)
+action_29=Action.find_or_create_by(name: "Comentar Pregunta", points: 7)
+action_30=Action.find_or_create_by(name: "Comentar Respuesta", points: 8)
+action_31=Action.find_or_create_by(name: "Responder Pregunta", points: 9)
+action_32=Action.find_or_create_by(name: "Denunciar Pregunta", points: 10)
+action_33=Action.find_or_create_by(name: "Denunciar Respuesta", points: 11)
+action_34=Action.find_or_create_by(name: "Denunciar Comentario", points: 12)
 level_2.actions << action_01
 level_2.actions << action_02
 level_2.actions << action_03
@@ -75,6 +75,9 @@ level_2.actions << action_28
 level_2.actions << action_29
 level_2.actions << action_30
 level_2.actions << action_31
+level_2.actions << action_32
+level_2.actions << action_33
+level_2.actions << action_34
 
 level_1.actions << action_01
 
@@ -88,7 +91,7 @@ etiqueta_electronica=Label.find_or_create_by(title: "Electronica", active: false
 puts "Se crean los usuarios Lucas, Mono, Vincent"
 User.create(name: "Lucas", lastname: "Camino", email: "lucasca95@yahoo.com.ar", password:"12345678", points: 10, level_id: 1)
 User.create(name: "Lihuen", lastname: "Figueroa", email: "tumonitosexy@msn.com.ar", password:"1235678", points: 10, level_id: 1)
-User.create(name: "Vincent", lastname: "Corrao", email: "licenciado@iscl.com.mx", password:"12345678", points: 10, university_id: 1, level_id: 1)
+User.create(name: "Vincent", lastname: "Corrao", email: "licenciado@iscl.com.mx", password:"12345678", points: 10, university_id: 1, level_id: 2)
 
 puts "Creo Notificaciones"
 Notification.find_or_create_by(body: "Notificaion 1", user_id: 3, read:true)
@@ -97,14 +100,14 @@ Notification.find_or_create_by(body: "Notificaion 3", user_id: 3, read:false)
 Notification.find_or_create_by(body: "Notificaion 1", user_id: 2, read:false)
 
 puts "Creo Pregunta"
-pregunta_1=Question.find_or_create_by(title: "¿Que hace Ruby?", body: "Serian tan amables de contrame lo que hace ruby?", user_id: 3)
+pregunta_1=Question.find_or_create_by(title: "¿Que hace Ruby?", body: "Serian tan amables de contrame lo que hace ruby?", user_id: 1)
 pregunta_1.labels << etiqueta_ingenieria
 puts "Creo Respuesta"
-Answer.find_or_create_by(body: "La verdad que preguntas como el culo", user_id: 3, question_id:1)
+Answer.find_or_create_by(body: "La verdad que preguntas como el culo", user_id: 2, question_id:1)
 
 puts "Creo Comentarios"
-Comment.find_or_create_by(body: "Comento Pregunta", user_id: 3, commentable_id: 1, commentable_type: "Question")
-Comment.find_or_create_by(body: "Comento Respuesta", user_id: 3, commentable_id: 1, commentable_type: "Answer")
+Comment.find_or_create_by(body: "Comento Pregunta", user_id: 2, commentable_id: 1, commentable_type: "Question")
+Comment.find_or_create_by(body: "Comento Respuesta", user_id: 1, commentable_id: 1, commentable_type: "Answer")
 
 puts "Creo Denuncias"
 Report.find_or_create_by(reason: "Denuncia Pregunta", user_id: 3, reportable_id: 1, reportable_type: "Question")

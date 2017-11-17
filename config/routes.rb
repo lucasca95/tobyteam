a42e922147fc0147ce536b76dd529f2f460d59e6
LihuenFigueroa <lihuenfigueroa@gmail.com>
2017-11-16 22:56:20 -0300

    Nada de nada

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   resources :universities
   resources :users
   get '/error', to: 'main#error'
-
+  
   root :to => 'questions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/oxigen_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class OxigenControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

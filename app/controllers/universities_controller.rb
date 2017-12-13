e945442801d330887f5e10a9458aee8c90be3e54
lucasca95 <lucasca95@yahoo.com.ar>
2017-12-13 19:44:45 -0300

    listo el crear universidad

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -1,6 +1,13 @@
 class UniversitiesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+
+
   def new
+    if ( current_user.permit("Administrar") )
+      @university = University.new
+    else
+      redirect_to :root, :alert => "No tiene permisos para Administrar"
+    end
   end
 
   def edit
@@ -14,6 +21,14 @@ class UniversitiesController < ApplicationController
   end
 
   def create
+    @university = University.new(params.require(:university).permit(:name, :province, :url))
+    @university.name = @university.name.upcase
+    @university.province = @university.province.upcase
+    if @university.save
+      redirect_to universities_path
+    else
+      redirect_to new_university_path, :alert =>"Ocurrio un error al guardar la universidad"
+    end
   end
 
   def update
--- app/models/university.rb
+++ app/models/university.rb
@@ -1,3 +1,18 @@
 class University < ApplicationRecord
 	has_many :users
+	
+
+	#Validaciones
+	validates :name,
+		presence: true
+	validates :name, 
+		format: { with: /\A([a-zA-Z]+(?: +[a-zA-Z])?)+\z/, message: ": sólo se permiten letras en el nombre" }
+
+	validates :province,
+		presence: true
+	validates :province, 
+		format: { with: /\A([a-zA-Z]+(?: +[a-zA-Z])?)+\z/, message: ": sólo se permiten letras en el nombre" }
+
+	validates :url,
+		presence: true
 end

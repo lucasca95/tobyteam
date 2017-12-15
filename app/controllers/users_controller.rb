c39a6734494e855bc036465bcf022a629dfefb84
lucasca95 <lucasca95@yahoo.com.ar>
2017-12-14 21:03:39 -0300

    ocultados botones de votar si estoy en pregunta/respuesta propia

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,28 +28,28 @@ class UsersController < ApplicationController
 
   def votar_pregunta 
     q = Question.find(params[:question])
-    like = params[:like]
-    if like
-      if current_user.permit("Votar Pregunta Positivo")
-        if !(q.vote(like, current_user))
-          redirect_to q, :alert => "Error al guardar los cambios"
-        else
-          redirect_to q
-        end
-      else
-         redirect_to q, :alert => "No tiene Permisos"
-      end
-    else
-        if current_user.permit("Votar Pregunta Negativo")
+      like = params[:like]
+      if like
+        if current_user.permit("Votar Pregunta Positivo")
           if !(q.vote(like, current_user))
             redirect_to q, :alert => "Error al guardar los cambios"
           else
             redirect_to q
           end
+        else
+          redirect_to q, :alert => "No tiene Permisos"
+        end
       else
-         redirect_to q, :alert => "No tiene Permisos"
+          if current_user.permit("Votar Pregunta Negativo")
+            if !(q.vote(like, current_user))
+              redirect_to q, :alert => "Error al guardar los cambios"
+            else
+              redirect_to q
+            end
+          else
+          redirect_to q, :alert => "No tiene Permisos"
+          end
       end
-    end
   end
 
   def votar_respuesta
